b3308d95b5f092d585c68fa1fd179a44b59410db
ismaelhtmljs <107575663+ismaelhtmljs@users.noreply.github.com>
2025-02-23 23:56:04 -0500

    agregando href al footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,10 +5,10 @@ function Footer() {
         <footer className="p-10 bg-footer">
             <div className="text-center flex flex-col gap-7 text-[14px]">
                 <div className="flex justify-center gap-3">
-                    <a href="#" className='hover:text-cyan-400'>Home</a>
-                    <a href="#" className='hover:text-cyan-400'>ABOUT US</a>
-                    <a href="#" className='hover:text-cyan-400'>PROJECTS</a>
-                    <a href="#" className='hover:text-cyan-400'>CONTACT</a>
+                    <a href="#Home" className='hover:text-cyan-400'>Home</a>
+                    <a href="#AboutUs" className='hover:text-cyan-400'>ABOUT US</a>
+                    <a href="#Projects" className='hover:text-cyan-400'>PROJECTS</a>
+                    <a href="#contact" className='hover:text-cyan-400'>CONTACT</a>
                 </div>
                 <div className='flex flex-col gap-2'>
                     <p>Creador y Programador Web : Ismael Guerrero</p>
